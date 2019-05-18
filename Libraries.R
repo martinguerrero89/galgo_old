de2100b2251e17e6ea50845bed9673a43c3d733d
martinguerrero89 <martinguerrero89@users.noreply.github.com>
2019-05-18 15:58:07 -0300

    Create Libraries.R

---
+++ Libraries.R
@@ -0,0 +1,28 @@
+##libraries
+
+require(parallel) #For parallelization of individual solutions
+require(doParallel)
+require(cba) #for subset of matrix distance
+require(cluster)
+require(survival)
+require(gplots)
+require(nsga2R) #For nsgaII
+require(gpuR) #For GPU usage
+require(matchingR) #For solving gale-shapley algorithm (stable marriage problem)
+require(Biobase)
+require(caret)
+require(illuminaio) #Read BGX files for illumina chips
+require(survcomp)
+require(genefu)
+
+require(survminer)
+require(MetaGxBreast)
+require(GEOquery)
+require(a4Base)
+require(gplots)
+require(xlsx)
+require(SummarizedExperiment)
+require(TCGAbiolinks)
+require(limma)
+require(edgeR)
+require(hgu133plus2.db)

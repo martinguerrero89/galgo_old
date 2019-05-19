42682a3143df39711c9fb03712641c73a01558e9
martinguerrero89 <martinguerrero89@users.noreply.github.com>
2019-05-19 13:31:12 -0300

    Create Galgo_CRC.R

---
+++ Galgo_CRC.R
@@ -0,0 +1,441 @@
+
+#Run galgo
+
+population= 300                   #Number of individuals to evaluate
+generations=500                 #Number of generations
+nCV=5                             #Number of crossvalidations for function "crossvalidation"
+plotgr=FALSE                      #set to TRUE if you want a generation plot
+GPU= TRUE                         # to use gpuR
+distancetype="pearson"          #Options are: "pearson","uncentered","spearman","euclidean"
+TournamentSize=2
+period=1825
+
+
+trainSet=esets[["GSE39582"]] ; h="CRC"
+prob_matrix= exprs(trainSet)
+clinical=pData(trainSet)
+OS=Surv(time=clinical$time,event=clinical$status)
+chrom_length= nrow(prob_matrix)   #length of chromosome
+
+
+
+dir.create(paste0("./Results/Results_",h),recursive=TRUE)
+resultdir=paste0("./Results/Results_",h)
+source("./Functions/Geneticalg.R")
+
+
+resultdir=paste0("./Results/Results","_",h,"/")
+file= paste0("results",500,".rda")
+load(paste0(resultdir,file))
+X1=output$Solutions
+PARETO=output$ParetoFront
+dots=do.call(rbind,PARETO)
+
+rownames(X1)=paste("result",1:nrow(X1),sep=".")
+
+
+plot(dots)
+lines(X1[names(which(X1[order(X1[,(chrom_length+2)]),"rnkIndex"]==1)),(chrom_length+2):(chrom_length+3)],col="red")
+
+
+RES=data.frame(solution=as.character(),k=as.numeric(),ngenes=as.numeric(),trainSil=as.numeric(),trainC=as.numeric(),stringsAsFactors = FALSE)
+
+
+for(i in 1:sum(X1[,"rnkIndex"]==1)){
+
+  RESULT=names(which(X1[order(X1[,(chrom_length+2)]),"rnkIndex"]==1))[i]
+  R=as.logical(X1[RESULT,1:chrom_length])
+  k=X1[RESULT,"k"]
+  D=mydist(prob_matrix[R,])
+  
+  
+  hsp_class=mycluster2(D,k)
+  C=kcentroid(prob_matrix[R,],hsp_class$cluster)
+  hsp_class= as.factor(classify(prob_matrix[R,],C,method=distancetype))
+  hsp_classdf= as.data.frame(hsp_class)
+  
+  mysurv <- OS
+  tumortotal <- survfit(mysurv~ hsp_classdf$hsp_class)
+  totalsdf <- survdiff(mysurv~ hsp_classdf$hsp_class)
+  tumortotalpval <- 1 - pchisq(totalsdf$chisq, length(totalsdf$n) - 1)
+  tumortotalpval <- format(tumortotalpval, digits=4)
+  COLS= levels(hsp_classdf[,"hsp_class"])
+  
+  
+  coxsimple=coxph(mysurv~hsp_class,data=hsp_classdf)
+  a=concordance.index(predict(coxsimple),surv.time=OS[,1],surv.event=OS[,2],outx=FALSE)$c.index
+  
+  c=mean(silhouette(as.numeric(hsp_class),D)[,3])
+  
+  row= c(RESULT,k,sum(R),c,a)
+  RES[nrow(RES)+1,]=row
+  print(row)
+  
+  }
+  
+  require(a4Base)
+  
+  testSol=NULL
+  for(i in unique(RES$k)){
+  testSol=c(testSol,RES[RES$k==i,"solution"][which.max(RES[RES$k==i,"trainC"])])
+    }
+  
+  
+  galgo=NULL
+  CentroidsList=list()
+  for(j in testSol){
+  RESULT=j
+  R=as.logical(X1[RESULT,1:chrom_length])
+  k=X1[RESULT,"k"]
+  D=mydist(prob_matrix[R,])
+  hsp_class=mycluster2(D,k)$cluster
+  hsp_classdf= data.frame(hsp_class=as.factor(hsp_class))
+  coxsimple=coxph(mysurv~hsp_class,data=hsp_classdf)
+  C=kcentroid(prob_matrix[R,],hsp_class)
+  labels=rank(c(0,coxsimple$coefficients))
+  colnames(C)= labels
+  CentroidsList[[j]]=C
+  name= paste(k,j,sep="_")
+  galgo=c(galgo,name)
+    for(i in downl){
+      hsp_class= as.factor(classify(exprs(esets[[i]])[R,],C,method=distancetype))
+      hsp_classdf= data.frame(hsp_class=hsp_class)
+      pred= predict(coxsimple,hsp_classdf)
+      levels(hsp_class)=labels
+      hsp_class = factor(hsp_class,levels(hsp_class)[order(labels)])
+      pData(esets[[i]])[,name]=hsp_class
+      pData(esets[[i]])[,paste0(name,".Pred")]=pred
+      }
+  }
+  
+SSP= c("CMS_Final")
+  for(j in SSP){
+  class=clinical[,j]
+  classdf= data.frame(class=as.factor(class))
+  coxsimple=coxph(OS~class,data=classdf)
+     for(i in downl){
+      class= pData(esets[[i]])[,j]
+      classdf= data.frame(class=class)
+      pred= predict(coxsimple,classdf)
+      pData(esets[[i]])[,paste0(j,".Pred")]=pred
+      }
+  }
+  
+
+  
+riskP=c(paste0(c(galgo,SSP),".Pred")) 
+resMatrix <- as.list(NULL)
+for(i in downl){
+res=pData(esets[[i]])
+SURV=Surv(res[,"time"],res[,"status"])
+
+for (Dat in riskP){
+  x=t(res[,Dat])
+  cindex <- t(apply(X=x, MARGIN=1, function(x, y) {
+    tt <- concordance.index(x=x, surv.time=y[,1], surv.event=y[,2], method="noether",outx=FALSE, na.rm=TRUE);
+    return(c("cindex"=tt$c.index, "cindex.se"=tt$se, "lower"=tt$lower, "upper"=tt$upper)) },
+    y=SURV))
+  resMatrix[[Dat]] <- rbind(resMatrix[[Dat]], cindex)
+  }
+}
+
+lapply(resMatrix,colMeans)
+
+ccmData <- tt <- rr <- NULL
+
+for(i in downl){
+  CIlist <- t(apply(X=pData(esets[[i]])[,riskP], MARGIN=2, function(x, y, z) {
+     tt <- concordance.index(x=x, surv.time=y, surv.event=z, method="noether", outx=FALSE, na.rm=TRUE);
+     return(tt); }, y=pData(esets[[i]])$time, z=pData(esets[[i]])$status))
+  assign(i,CIlist)
+  }
+
+for(i in 1:length(riskP)){
+ tt <- NULL
+ listOne <- list()
+ for(j in downl){
+ listOne[[j]]=get(j)[[i]]
+ }
+ for(z in 1:length(riskP)){
+ listTwo <- list()
+ for(j in downl){
+  listTwo[[j]]=get(j)[[z]]
+  }
+ rr <- cindex.comp.meta(list.cindex1=listOne, list.cindex2=listTwo)
+ tt <- cbind(tt, rr$p.value) ##list(round(rr$p.value,5)))
+ }
+ccmData <- rbind(ccmData, tt)
+}
+
+ccmData <- as.data.frame(ccmData)
+colnames(ccmData) <- riskP
+rownames(ccmData) <- riskP
+
+
+resMatrixForest=resMatrix
+for( i in riskP){
+tt=combine.est(as.numeric(resMatrixForest[[i]][,"cindex"]),as.numeric(resMatrixForest[[i]][,"cindex.se"]))
+tt$lower <- tt$estimate + qnorm(0.025, lower.tail=TRUE) * tt$se
+tt$upper <- tt$estimate + qnorm(0.025, lower.tail=FALSE) * tt$se
+resMatrixForest[[i]]=rbind(resMatrixForest[[i]],unlist(tt))
+
+resMatrixForest[[i]]=cbind(resMatrixForest[[i]],dataset=c(downl,"Combined"))
+resMatrixForest[[i]]=cbind(resMatrixForest[[i]],signature=i)
+}
+
+RR_data= as.data.frame(do.call(rbind,resMatrixForest),stringsAsFactors=FALSE)
+RR_data$cindex=as.numeric(RR_data$cindex)
+RR_data$lower=as.numeric(RR_data$lower)
+RR_data$upper=as.numeric(RR_data$upper)
+RR_data$dataset <- factor(RR_data$dataset, levels = c(downl,"Combined"))
+p = ggplot(data=RR_data,
+    aes(x = signature,y = cindex, ymin = lower, ymax = upper ))+
+    geom_pointrange(aes(col=signature))+
+    geom_hline(aes(fill=signature),yintercept =0.5, linetype=2)+
+    xlab('Signature for each dataset')+ ylab("C.Index (95% Confidence Interval)")+
+    geom_errorbar(aes(ymin=lower, ymax=upper,col=signature),width=0.5,cex=1)+ 
+    facet_wrap(~dataset,strip.position="left",nrow=9,scales = "free_y") +
+    theme(plot.title=element_text(size=16,face="bold"),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.x=element_text(face="bold"),
+        axis.title=element_text(size=12,face="bold"),
+        strip.text.y = element_text(hjust=0,vjust = 1,angle=180,face="bold"))+
+    coord_flip()
+ p
+
+Comb=combineTwoExpressionSet(esets[[2]],esets[[3]])
+for(i in 3:length(downl[-which(downl=="GSE39582")])){
+  Comb=combineTwoExpressionSet(Comb,esets[[i]])
+}
+
+
+
+library(survminer)
+  tumortotal1 <- survfit(Surv(Comb$time,Comb$status)~ Comb$CMS_Final)
+  tumortotal2 <- survfit(Surv(Comb$time,Comb$status)~ Comb$`2_result.14`)
+  tumortotal1diff <- survdiff(Surv(Comb$time,Comb$status)~ Comb$CMS_Final)
+  tumortotal2diff <- survdiff(Surv(Comb$time,Comb$status)~ Comb$`2_result.14`)
+  tumortotal1pval<- pchisq(tumortotal1diff$chisq, length(tumortotal1diff$n) - 1, lower.tail = FALSE) 
+  tumortotal2pval<- pchisq(tumortotal2diff$chisq, length(tumortotal2diff$n) - 1, lower.tail = FALSE) 
+  
+  
+  SURV=list(CMS=tumortotal1, GALGO=tumortotal2)
+  COLS=c(1:8,10)
+  par(cex=1.35, mar=c(3.8, 3.8, 2.5, 2.5) + 0.1)
+  p=ggsurvplot(SURV,combine=TRUE,data=Comb,risk.table=TRUE,pval=TRUE,palette="dark2", title="Combined Set \n Colon survival comparison", surv.scale="percent", conf.int=FALSE, xlab="time (days)", ylab="survival(%)", xlim=c(0,period),break.time.by = 365, ggtheme = theme_minimal(), risk.table.y.text.col = TRUE, risk.table.y.text = FALSE,censor=FALSE)
+  #pdf(pdfname, width=6, height=5, useDingbats=FALSE,onefile = FALSE)
+  print(p)
+
+
+  tumortotal1 <- survfit(Surv(esets[["GSE39582"]]$time,esets[["GSE39582"]]$status)~ esets[["GSE39582"]]$CMS_Final)
+  tumortotal2 <- survfit(Surv(esets[["GSE39582"]]$time,esets[["GSE39582"]]$status)~ esets[["GSE39582"]]$`2_result.14`)
+  tumortotal1diff <- survdiff(Surv(esets[["GSE39582"]]$time,esets[["GSE39582"]]$status)~ esets[["GSE39582"]]$CMS_Final)
+  tumortotal2diff <- survdiff(Surv(esets[["GSE39582"]]$time,esets[["GSE39582"]]$status)~ esets[["GSE39582"]]$`2_result.14`)
+  tumortotal1pval<- pchisq(tumortotal1diff$chisq, length(tumortotal1diff$n) - 1, lower.tail = FALSE) 
+  tumortotal2pval<- pchisq(tumortotal2diff$chisq, length(tumortotal2diff$n) - 1, lower.tail = FALSE) 
+  
+  
+  
+  SURV=list(CMS=tumortotal1, GALGO=tumortotal2)
+  par(cex=1.35, mar=c(3.8, 3.8, 2.5, 2.5) + 0.1)
+  p=ggsurvplot(SURV,combine=TRUE,data=Comb,risk.table=TRUE,pval=TRUE,palette="dark2", title="Train Set \n Colon survival comparison", surv.scale="percent", conf.int=FALSE, xlab="time (days)", ylab="survival(%)", xlim=c(0,period),break.time.by = 365, ggtheme = theme_minimal(), risk.table.y.text.col = TRUE, risk.table.y.text = FALSE,censor=FALSE)
+  print(p)
+
+
+finalSig="2_result.14"
+V=unlist(strsplit(finalSig,"_"))
+R=as.logical(X1[V[2],1:chrom_length])
+  k=as.numeric(V[1])
+  
+trainset="GSE39582"
+TrainExprs=exprs(esets[[trainset]])
+rownames(TrainExprs)= fData(esets[[trainset]])$gene  
+TrainClass= pData(esets[[trainset]])[,riskP]
+
+colfuncR <- colorRampPalette(rev(brewer.pal(11 , "Spectral" )))
+CMS= colfuncR(5)[as.numeric(as.factor(TrainClass[,riskP[4]]))]
+galgo2=colfuncR(2)[as.numeric(as.factor(TrainClass[,riskP[3]]))]
+galgo3=colfuncR(3)[as.numeric(as.factor(TrainClass[,riskP[2]]))]
+galgo4=colfuncR(4)[as.numeric(as.factor(TrainClass[,riskP[1]]))]
+
+
+COLS=data.frame(galgo2, galgo3,galgo4, CMS)
+
+CLASS=TrainClass[,paste(finalSig,"Pred",sep=".")]
+Ord= order(CLASS)
+library(gplots)
+
+colfunc <- colorRampPalette(c("blue", "white", "red"))
+breaks=seq(-2, 2, by=0.1) 
+
+breaks=append(breaks, 5)
+breaks=append(breaks, -5, 0)
+
+HM1=heatmap.3(TrainExprs[R,Ord],Colv= FALSE,hclustfun=myclust,dendrogram="none", distfun=mydist2,trace="none",scale="row",col=colfunc,breaks=breaks,ColSideColors =as.matrix(COLS[Ord,]),ColSideColorsSize=3)
+
+
+#Testset (Comb)
+
+
+TestExprs=ComBat(exprs(Comb),pData(Comb)$batch)
+rownames(TestExprs)= fData(Comb)$gene  
+TestClass= pData(Comb)[,riskP]
+
+colfuncR <- colorRampPalette(rev(brewer.pal(11 , "Spectral" )))
+CMS= colfuncR(5)[as.numeric(as.factor(TestClass[,riskP[4]]))]
+galgo2=colfuncR(2)[as.numeric(as.factor(TestClass[,riskP[3]]))]
+galgo3=colfuncR(3)[as.numeric(as.factor(TestClass[,riskP[2]]))]
+galgo4=colfuncR(4)[as.numeric(as.factor(TestClass[,riskP[1]]))]
+
+
+COLS=data.frame(galgo2, galgo3,galgo4, CMS)
+
+
+CLASS=TestClass[,paste(finalSig,"Pred",sep=".")]
+Ord= order(CLASS)
+
+
+HM2=heatmap.3(TestExprs[R,Ord][rev(HM1$rowInd),],Colv= FALSE,Rowv=FALSE,hclustfun=myclust,dendrogram="none", distfun=mydist2,trace="none",scale="row",col=colfunc,breaks=breaks,ColSideColors =as.matrix(COLS[Ord,]),ColSideColorsSize=3)
+
+
+
+library(xlsx)
+for(i in names(CentroidsList)){
+write.xlsx(CentroidsList[[i]], file="CRC_Galgo_Centroids.xlsx", sheetName=i,append=TRUE, row.names=TRUE)
+}
+
+
+write.xlsx(RR_data[,c(6,5,1:4)], file="CRC_CI_Meta.xlsx", sheetName="CI",append=TRUE, row.names=FALSE)
+write.xlsx(ccmData, file="CRC_CI_Meta.xlsx", sheetName="Comparison",append=TRUE, row.names=TRUE)
+
+
+
+#Gage pathway analysis
+
+library(gage)
+data(go.sets.hs)
+kegg.gs=go.sets.hs
+
+CLASS=as.factor(TrainClass[,paste(finalSig,"Pred",sep=".")])
+TrainExprs= exprs(esets[[trainset]])
+
+
+ind1<- which( CLASS == levels(CLASS)[1])
+ind2<- which( CLASS == levels(CLASS)[2])
+clust1.kegg.p <- gage(TrainExprs,ref=ind2, gsets = kegg.gs,compare="as.group")
+clust2.kegg.p <- gage(TrainExprs,ref=ind1, gsets = kegg.gs,compare="as.group")
+
+##Sort and count signficant gene sets based on q- or p-value cutoffs:
+##plot heatmap by clust
+
+clust1.kegg.p.sig<- sigGeneSet(clust1.kegg.p, outname = "clust1.kegg", heatmap = FALSE)
+clust2.kegg.p.sig<- sigGeneSet(clust2.kegg.p, outname = "clust2.kegg", heatmap = FALSE)
+
+## extract a non-redundant signcant gene set list
+
+# subtype I #
+clust1.kegg.p.esg.up <- esset.grp(clust1.kegg.p$greater,
+                                  TrainExprs,ref=ind2, gsets = kegg.gs,
+                                  outname = "clust1.kegg.up",
+                                  test4up = TRUE, output = TRUE,compare="as.group")
+                                  
+
+clust1.kegg.p.esg.dn <- esset.grp(clust1.kegg.p$less,
+                                  TrainExprs,ref=ind2, gsets = kegg.gs, 
+                                  test4up = FALSE, output = TRUE, 
+                                  outname = "clust1.kegg.dn",compare="as.group")
+
+g1<- clust1.kegg.p.esg.up$essentialSets[1:7]
+l1<- clust1.kegg.p.esg.dn$essentialSets[1:7]
+
+# subtype II #
+
+clust2.kegg.p.esg.up <- esset.grp(clust2.kegg.p$greater,
+                                 TrainExprs,ref=ind1, gsets = kegg.gs,
+                                  outname = "clust2.kegg.up",
+                                  test4up = TRUE, output = TRUE,compare="as.group")
+
+clust2.kegg.p.esg.dn <- esset.grp(clust2.kegg.p$less,
+                                  TrainExprs,ref=ind1, gsets = kegg.gs, 
+                                  test4up = FALSE, output = TRUE, 
+                                  outname = "clust2.kegg.dn",compare="as.group")
+
+g2<- clust2.kegg.p.esg.up$essentialSets[1:7]
+l2<- clust2.kegg.p.esg.dn$essentialSets[1:7]
+
+Paths<- c(g1,g2,l1,l2)
+Paths<- unique(c(g1,g2,l1,l2))
+Path_name<- substring(Paths,12)
+
+bar1=data.frame(stat=clust1.kegg.p$stats[Paths,1],path=Path_name,subtype=1)
+bar1= bar1[order(bar1$stat),]
+bar2=data.frame(stat=clust2.kegg.p$stats[Paths,1],path=Path_name,subtype=2)
+bar2= bar2[order(bar2$stat),]
+
+
+bar=rbind(bar1,bar2)
+
+bar$path <- factor(bar$path, levels=unique(bar$path) )
+
+
+## heatmap data prep ##
+
+split_bar<- split(bar, bar$subtype)
+
+st_I<- as.data.frame(split_bar[1])
+st_I<- arrange(st_I, X1.path)
+
+st_II<- as.data.frame(split_bar[2])
+st_II<- arrange(st_II, X2.path)
+
+heatmap_table<- as.data.frame(cbind(st_I$X1.stat, st_II$X2.stat))
+colnames(heatmap_table)<- c("Subtype I", "Subtype II")
+rownames(heatmap_table)<- as.character(st_I$X1.path)
+
+## heatmap ##
+colfuncR <-  colorRampPalette(rev(brewer.pal(11,"RdBu")))
+col<-colfuncR(100)
+
+
+ heatmap.3(as.matrix(heatmap_table), col= col, trace="none",
+           sepcolor="white",
+           colsep=1:ncol(heatmap_table),
+           rowsep=1:nrow(heatmap_table),
+           dendrogram = "none",margin=c(6,28),cexRow=1,cexCol = 1)
+
+
+
+###Comparison with random signatures
+
+CdistColon=list()
+CresColon= list()
+for(j in testSol){
+iter=1000
+
+
+k=RES[RES$solution==j,"k"]
+ngenes=RES[RES$solution==j,"ngenes"]
+W=matrix(0,nrow=iter,ncol=chrom_length)
+for(it in 1:iter){
+  ind=sample(1:chrom_length,ngenes,replace=F)
+  W[it,ind]=1
+}
+
+Cdist=NULL
+for(it in 1:iter){
+R=as.logical(W[it,])
+D=mydist(prob_matrix[R,])
+  hsp_class=as.factor(mycluster2(D,k)$cluster)
+  coxsimple=coxph(OS~hsp_class)
+  pred= predict(coxsimple)
+  cind= concordance.index(x=pred, surv.time=OS[,1], surv.event=OS[,2], method="noether",outx=FALSE, na.rm=TRUE)$c.index
+  Cdist=c(Cdist,cind)
+}
+
+Name=paste("Colon",j,sep=".")
+CdistColon[[Name]]=Cdist
+CresColon[[Name]]= as.numeric(RES[RES$solution==j,"trainC"])
+
+}
+
